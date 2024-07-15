c01e686d7385b19aed28e897faea62e39193badd
DevdattKhilari <devdattkhilari@gmail.com>
2024-07-15 15:52:18 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,234 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  // {
+  //   imageUrl: motion,
+  //   name: "Motion",
+  //   type: "Animation",
+  // },
+  // {
+  //   imageUrl: mui,
+  //   name: "Material-UI",
+  //   type: "Frontend",
+  // },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  // {
+  //   imageUrl: sass,
+  //   name: "Sass",
+  //   type: "Frontend",
+  // },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Frontend Web Developer",
+    company_name: "CodeSoft",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "December 2023 - January 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  }
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#fbc3bc",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#b7e4c7",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#a2d2ff",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/DevdattKhilari",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/devdatt-khilari-b5b00a229/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Brain Blast Web-App",
+    description:
+      "Brain Blast Quiz is a fun and engaging quiz app that challenges your knowledge across various topics. Test your skills, learn new facts, and compete with friends!",
+    link: "https://github.com/DevdattKhilari/Brain_Blast",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Finance_Stock_Market_Model",
+    description:
+      "The Stock Price Prediction App is a Streamlit-based web application that provides users with tools to analyze historical stock price data, visualize technical indicators, and make short-term price predictions using different machine learning models.",
+    link: "https://github.com/DevdattKhilari/Hash_IT_Hackthon_Finance_Stock_Market_Model",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Pet-Shelter ",
+    description:
+      "Connecting furry friends with loving homes, our pet shelter site is a compassionate platform where adoption dreams come true. Browse, adopt, and bring joy to a pet in need.",
+    link: "https://github.com/DevdattKhilari?tab=repositories",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Pathfinder-website",
+    description:
+      "A responsive website for Pathfinder, showcasing project features, resources, and user interactions using Next.js.",
+    link: "https://github.com/DevdattKhilari/Pathfinder-website",
+  },
+  // {
+  //   iconUrl: estate,
+  //   theme: "btn-back-black",
+  //   name: "Social App",
+  //   description:
+  //     "Developed a web application for social media user. User can read write or post their beautiful moment.",
+  //   link: "https://github.com/Raihanhn/Social_app",
+  // },
+  // {
+  //   iconUrl: summiz,
+  //   theme: "btn-back-yellow",
+  //   name: "Grocery Store",
+  //   description:
+  //     "A beautiful website where user can find out best deals for their daily basis grocery products and can read articles or blogs.",
+  //   link: "https://github.com/Raihanhn/Grocery-Store",
+  // },
+];
